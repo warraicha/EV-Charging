c32bc5786fb7b29c38092043294511c0391cbfba
warraicha <abdullah.warraich@informed.com>
2025-01-10 15:53:17 +0000

    Refactoring

--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -7,24 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 const axios = require('axios');
 
-// Add your routes here
-// Run this code when a form is submitted to 'adequate-onstreet-parking-answer'
 router.post('/adequate-onstreet-parking-answer', function (req, res) {
 
-    // Make a variable and give it the value from 'adequate-onstreet-parking'
     var adequate = req.session.data['adequate-onstreet-parking']
   
-    // Check whether the variable matches a condition
     if (adequate == "Yes"){
-      // Send user to next page
       res.redirect('/enter-VRN')
     } else {
-      // Send user to ineligible page
       res.redirect('/ineligible')
     }
 })
 
-// Route to handle form submission from "check-answers" page
 router.post('/submit-application', async function (req, res) {
   const selectedAddress = req.session.data['address'];
 
@@ -45,7 +38,6 @@ router.post('/submit-application', async function (req, res) {
   };
 
   try {
-    // Make a POST request to the backend API
     const response = await axios.post('http://localhost:5016/api/applications', requestBody);
 
     console.log('Application submitted successfully:', response.data);
